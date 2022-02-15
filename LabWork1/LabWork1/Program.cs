849155706e54ef2e425b861c488c49c357ec13b0
MerkDab <merkdab@mail.ru>
2022-02-15 14:23:28 +0300

    firstcommit

---
+++ LabWork1/LabWork1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое неотрицательное число:");
+int someNum = Convert.ToInt32(Console.ReadLine());
+someNum += 5;
+someNum *= 2;
+Console.WriteLine(someNum);
+Random rnd = new Random();
+int[] someMassiv = new int[someNum];
+for (int i = 0; i < someMassiv.Length; i++)
+{
+    someMassiv[i] = rnd.Next(0, 100);
+    Console.WriteLine(someMassiv[i]);
+}
